9ceff0aeb1485244d80cdfc74104bc6b49c8dda6
Xiaoyi Liu <lxy878@gmail.com>
2020-08-30 16:48:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const tutorials = [
 const titleCased = () => {
   return tutorials.map(question => {
     const words = question.split(' ');
-    return words.map(word=> word.charAt(0).toUpperCase()+word.slice(1)).join(',');
+    return words.map(word=> word.charAt(0).toUpperCase()+word.slice(1)).join(' ');
   });
 }
 console.log(titleCased())
